e60a4554a36a0b35b1d2a87f5240bbfd8f6e67f6
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-03-08 09:51:29 +0800

    add BS1

---
+++ evidence/BS1.go
@@ -0,0 +1,46 @@
+package evidence
+
+import (
+	"fmt"
+	"os"
+)
+
+var BS1MorbidityThreshold = 0.01
+var BS1AFList = []string{
+	"1000G AF",
+	"ExAC AF",
+	"GnomAD AF",
+}
+
+// ture	:	"1"
+// flase:	"0"
+// nil	:	""
+func CheckBS1(item map[string]string) string {
+	if CheckAFAllLowThen(item, BS1AFList, BS1MorbidityThreshold, true) {
+		return "0"
+	} else {
+		return "1"
+	}
+}
+
+func CompareBS1(item map[string]string) {
+	rule := "BS1"
+	val := CheckBS1(item)
+	if val != item[rule] {
+		if item[rule] == "0" && val == "" {
+		} else {
+			fmt.Fprintf(
+				os.Stderr,
+				"Conflict %s:[%s] vs [%s]\t%s[%s]\n",
+				rule,
+				val,
+				item[rule],
+				"MutationName",
+				item["MutationName"],
+			)
+			for _, key := range BS1AFList {
+				fmt.Fprintf(os.Stderr, "\t%s:[%s]\n", key, item[key])
+			}
+		}
+	}
+}
