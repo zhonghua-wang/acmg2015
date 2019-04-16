97478a7ecb6b94f75ee886225dec2f8417fea285
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-16 15:50:19 +0800

    use MapInt

--- acmg2015.go
+++ acmg2015.go
@@ -12,7 +12,7 @@ var (
 )
 
 func AddACMG2015(inputData map[string]string) map[string]string {
-	var LOFIntoleranceGeneList = simple_util.JsonFile2Map("db/LOFIntoleranceGeneList.json")
+	var LOFIntoleranceGeneList = simple_util.JsonFile2MapInt("db/LOFIntoleranceGeneList.json")
 	var info = make(map[string]string)
 	info["PVS1"] = evidence.CheckPVS1(inputData, LOFIntoleranceGeneList)
 	inputData["ACMG"] = PredACMG2015(info)
