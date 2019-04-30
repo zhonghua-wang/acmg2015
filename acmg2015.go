dbd01ecfa682e541924e70605c74f891702466b9
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-30 16:53:04 +0800

    remove break api

--- acmg2015.go
+++ acmg2015.go
@@ -1,8 +1,6 @@
 package acmg2015
 
 import (
-	"github.com/liserjrqlxue/acmg2015/evidence"
-	"github.com/liserjrqlxue/simple-util"
 	"regexp"
 )
 
@@ -12,9 +10,9 @@ var (
 )
 
 func AddACMG2015(inputData map[string]string) map[string]string {
-	var LOFIntoleranceGeneList = simple_util.JsonFile2MapInt("db/LOFIntoleranceGeneList.json")
+	//var LOFIntoleranceGeneList = simple_util.JsonFile2MapInt("db/LOFIntoleranceGeneList.json")
 	var info = make(map[string]string)
-	info["PVS1"] = evidence.CheckPVS1(inputData, LOFIntoleranceGeneList)
+	//info["PVS1"] = evidence.CheckPVS1(inputData, LOFIntoleranceGeneList)
 	inputData["ACMG"] = PredACMG2015(info)
 	return inputData
 }
