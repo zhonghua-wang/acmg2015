4204fd03a015be9c3a1c8c1d8881b8c68303aa68
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-04-10 16:55:51 +0800

    add BA1.exception.list

--- evidence/BA1.go
+++ evidence/BA1.go
@@ -1,5 +1,7 @@
 package evidence
 
+import "github.com/liserjrqlxue/goUtil/textUtil"
+
 var BA1AFThreshold = 0.05
 var BA1AFList = []string{
 	"ESP6500 AF",
@@ -10,10 +12,22 @@ var BA1AFList = []string{
 	"GnomAD EAS AF",
 }
 
+func CreateBA1Exception(fileName string) (BA1Exception map[string]bool) {
+	BA1Exception = make(map[string]bool)
+	for _, key := range textUtil.File2Array(fileName) {
+		BA1Exception[key] = true
+	}
+	return
+}
+
 // ture	:	"1"
 // flase:	"0"
 // nil	:	""
-func CheckBA1(item map[string]string) string {
+func CheckBA1(item map[string]string, BA1Exception map[string]bool) string {
+	var key = item["Transcript"] + ":" + item["cHGVS"]
+	if BA1Exception[key] {
+		return "0"
+	}
 	if CheckAFAllLowThen(item, BA1AFList, BA1AFThreshold, true) {
 		return "0"
 	} else {
@@ -21,9 +35,9 @@ func CheckBA1(item map[string]string) string {
 	}
 }
 
-func CompareBA1(item map[string]string, lostOnly bool) {
+func CompareBA1(item map[string]string, BA1Exception map[string]bool, lostOnly bool) {
 	rule := "BA1"
-	val := CheckBA1(item)
+	val := CheckBA1(item, BA1Exception)
 	if val != item[rule] {
 		if item[rule] == "0" && val == "" {
 		} else {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/brentp/bix v0.0.0-20190718140914-00aa7a7f205d
 	github.com/brentp/irelate v0.0.0-20180801151701-1bf7c8f95c75
 	github.com/brentp/vcfgo v0.0.0-20190824021612-654ed2e5945d // indirect
+	github.com/liserjrqlxue/goUtil v0.0.10
 	github.com/liserjrqlxue/parse-gff3 v0.0.0-20190425064232-1e45be11c625
 	github.com/liserjrqlxue/simple-util v1.0.1
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
