80ecd0a599955331c5c4c85aa2917e973cb03ffd
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-04-16 17:18:33 +0800

    update PM2

--- evidence/PM2.go
+++ evidence/PM2.go
@@ -1,8 +1,9 @@
 package evidence
 
 var (
-	PM2ARAFThreshold = 0.005
-	PM2ADAFThreshold = 0.0
+	PM2ARAFThreshold  = 0.005
+	PM2ADAFThreshold  = 0.0
+	PM2ADAFThreshold2 = 0.00001
 )
 
 var PM2AFList = []string{
@@ -15,27 +16,32 @@ var PM2AFList = []string{
 }
 
 // PM2
-func CheckPM2(item map[string]string) string {
+func CheckPM2(item map[string]string, BS2GeneList map[string]bool) string {
 	inherit := item["ModeInheritance"]
-	if isARXLNA.MatchString(inherit) || inherit == "" {
+	if isARDRXLNA.MatchString(inherit) || inherit == "" {
 		if CheckAFAllLowThen(item, PM2AFList, PM2ARAFThreshold, true) {
 			return "1"
 		} else {
 			return "0"
 		}
-	} else if isAD.MatchString(inherit) {
-		if CheckAFAllLowThen(item, PM2AFList, PM2ADAFThreshold, true) {
-			return "1"
+	} else if isADYL.MatchString(inherit) {
+		if BS2GeneList[item["Gene Symbol"]] {
+			if CheckAFAllLowThen(item, PM2AFList, PM2ADAFThreshold2, true) {
+				return "1"
+			}
 		} else {
-			return "0"
+			if CheckAFAllLowThen(item, PM2AFList, PM2ADAFThreshold, true) {
+				return "1"
+			}
 		}
+		return "0"
 	}
-	return ""
+	return "0"
 }
 
-func ComparePM2(item map[string]string) {
+func ComparePM2(item map[string]string, BS2GeneList map[string]bool) {
 	rule := "PM2"
-	val := CheckPM2(item)
+	val := CheckPM2(item, BS2GeneList)
 	if val != item[rule] {
 		PrintConflict(item, rule, val, append(PM2AFList, "ModeInheritance")...)
 	}
