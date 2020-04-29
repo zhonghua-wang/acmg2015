4f8e884ad9b6cff9a841f77ea54bce0528a50b5b
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-04-29 16:45:53 +0800

    update BP1

--- evidence/BP1.go
+++ evidence/BP1.go
@@ -1,59 +1,36 @@
 package evidence
 
 import (
-	"github.com/liserjrqlxue/simple-util"
-	"regexp"
+	"github.com/liserjrqlxue/goUtil/textUtil"
 )
 
 var (
-	BP1LoFRatioThreshold = 0.80
+	BP1geneList map[string]bool
 )
 
-func CalGeneLoFRatio(fileName, key string, filter *regexp.Regexp, threshold int) map[string]float64 {
-	var allCount = make(map[string]int)
-	var targetCount = make(map[string]int)
-	var ratioCount = make(map[string]float64)
-	itemArray, _ := simple_util.File2MapArray(fileName, "\t", nil)
-	for _, item := range itemArray {
-		if !filter.MatchString(item[key]) {
-			continue
-		}
-		gene := item["Gene Symbol"]
-		allCount[gene]++
-		if FuncInfo[item["Function"]] == 3 {
-			targetCount[gene]++
-		}
+func GetBP1geneList(fileName string) {
+	var genes = textUtil.File2Array(fileName)
+	BP1geneList = make(map[string]bool)
+	for _, gene := range genes {
+		BP1geneList[gene] = true
 	}
-	for key, val := range targetCount {
-		if threshold > 0 {
-			if allCount[key] >= threshold {
-				ratioCount[key] = float64(val) / float64(allCount[key])
-			}
-		} else {
-			ratioCount[key] = float64(val) / float64(allCount[key])
-		}
-	}
-	return ratioCount
 }
 
 // BP1
-func CheckBP1(item map[string]string, ClinVarBP1GeneList, HgmdBP1GeneList map[string]float64) string {
+func CheckBP1(item map[string]string) string {
 	if item["Function"] != "missense" {
 		return "0"
 	}
-	gene := item["Gene Symbol"]
-	if ClinVarBP1GeneList[gene] > BP1LoFRatioThreshold || HgmdBP1GeneList[gene] > BP1LoFRatioThreshold {
+	if BP1geneList[item["Gene Symbol"]] {
 		return "1"
 	} else {
 		return "0"
 	}
-
-	return ""
 }
 
 func CompareBP1(item map[string]string, ClinVarBP1GeneList, HgmdBP1GeneList map[string]float64) {
 	rule := "BP1"
-	val := CheckBP1(item, ClinVarBP1GeneList, HgmdBP1GeneList)
+	val := CheckBP1(item)
 	if val != item[rule] {
 		PrintConflict(item, rule, val, "Function", "Gene Symbol")
 	}
