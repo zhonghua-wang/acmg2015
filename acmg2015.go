4385bf87cce55e6ecb016c047c46a305cea6becf
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-05-08 10:13:13 +0800

    accept autoPVS1

--- acmg2015.go
+++ acmg2015.go
@@ -1,9 +1,9 @@
 package acmg2015
 
-func AddACMG2015(inputData map[string]string) map[string]string {
+func AddACMG2015(inputData map[string]string, autoPVS1 bool) map[string]string {
 	//var LOFIntoleranceGeneList = simple_util.JsonFile2MapInt("db/LOFIntoleranceGeneList.json")
 	var info = make(map[string]string)
 	//info["PVS1"] = evidence.CheckPVS1(inputData, LOFIntoleranceGeneList)
-	inputData["ACMG"] = PredACMG2015(info)
+	inputData["ACMG"] = PredACMG2015(info, autoPVS1)
 	return inputData
 }
