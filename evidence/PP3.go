31c5d7d6b065ab3da99f9a8ad4fed4bb9f09e034
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2021-03-10 16:53:46 +0800

    update PP3

--- evidence/PP3.go
+++ evidence/PP3.go
@@ -3,6 +3,7 @@ package evidence
 // ture	:	"1"
 // flase:	"0"
 func CheckPP3(item map[string]string, autoPVS1 bool) string {
+	// PVS1排除
 	if autoPVS1 {
 		switch item["AutoPVS1 Adjusted Strength"] {
 		case "VeryStrong":
@@ -17,6 +18,8 @@ func CheckPP3(item map[string]string, autoPVS1 bool) string {
 	} else if item["PVS1"] == "1" {
 		return "0"
 	}
+
+	// 保守性
 	var count = 0
 	for _, pred := range []string{
 		item["GERP++_RS_pred"],
@@ -33,9 +36,12 @@ func CheckPP3(item map[string]string, autoPVS1 bool) string {
 		return "0"
 	}
 
+	// 有害性
+	// b1
+	var b1 = true
 	count = 0
 	if isNeutral.MatchString(item["Ens Condel Pred"]) {
-		return "0"
+		b1 = false
 	} else if isDeleterious.MatchString(item["Ens Condel Pred"]) {
 		count++
 	}
@@ -45,28 +51,33 @@ func CheckPP3(item map[string]string, autoPVS1 bool) string {
 		item["Polyphen2 HVAR Pred"],
 	} {
 		if isP.MatchString(pred) || isI.MatchString(pred) {
-			return "0"
+			b1 = false
 		} else if isD.MatchString(pred) {
 			count++
 		}
 	}
-	if isSplice.MatchString(item["Function"]) || item["Function"] == "intron" {
-		for _, pred := range []string{
-			item["dbscSNV_RF_pred"],
-			item["dbscSNV_ADA_pred"],
-			item["SpliceAI Pred"],
-		} {
-			if isP.MatchString(pred) || isI.MatchString(pred) {
-				return "0"
-			} else if isD.MatchString(pred) {
-				count++
-			}
-		}
-		if item["Function"] != "splice-3" && item["Function"] != "splice-5" && isD.MatchString(item["SpliceAI Pred"]) {
-			return "1"
+	if count < 2 {
+		b1 = false
+	}
+
+	// b2
+	var b2 = true
+	count = 0
+	for _, pred := range []string{
+		item["dbscSNV_RF_pred"],
+		item["dbscSNV_ADA_pred"],
+		item["SpliceAI Pred"],
+	} {
+		if isP.MatchString(pred) || isI.MatchString(pred) {
+			b2 = false
+		} else if isD.MatchString(pred) {
+			count++
 		}
 	}
-	if count >= 2 {
+	if count < 2 && !isD.MatchString(item["SpliceAI Pred"]) {
+		b2 = false
+	}
+	if b1 || b2 {
 		return "1"
 	}
 	return "0"
